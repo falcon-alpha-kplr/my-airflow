f2a8b061cbb9a5ab2874eaaeaaefc0f07487f224
falcon alpha <falcon.alpha@training.kplr.fr>
2023-04-19 21:54:41 +0000

    un commentaire

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,142 +1,57 @@
-from datetime import datetime, timedelta
-from airflow import DAG, Dataset
-from airflow.operators.bash_operator import BashOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperator  # Tâches de création de table
-#4 Codes suivants pour : Tâche de récupération de données
-import os
+import json
+import pathlib
+
+import airflow.utils.dates
 import requests
-from airflow.decorators import task
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False    
-}
-
-my_dataset = Dataset('/tmp/data.csv')
-
-# Define the consumer DAG
-with DAG(dag_id='producer_aware', 
-        start_date = datetime.now(),
-        schedule_interval = timedelta(minutes=5),
-        default_args=default_args) as dag:
-
-    generate_dataset = BashOperator(
-        task_id='generate_dataset',
-        outlets=[my_dataset],
-        bash_command='echo "data1,data2,data3\n" >> /tmp/data.csv',
-    )
-
-    check_file_task = BashOperator(
-        task_id='check_file',
-        bash_command='cat /tmp/data.csv',
-    )
-
-    generate_dataset >> check_file_task
-
-# Define the consumer DAG
-with DAG(dag_id='consumer_aware', 
-        schedule = [my_dataset],
-        start_date = datetime.now(),
-        default_args=default_args) as dag:
-
-    # Define the task that consumes the dataset
-    consume_dataset = BashOperator(
-        task_id="consume_dataset",
-        bash_command="cat /tmp/data.csv",
-        retries=3,
-    )
-
-
-# Tâches de création de table
-"""
-Il est possible d'utiliser PostgresOperator afin de définir des tâches qui 
-créeront des tables dans la base de données postgres.
-
-Il est prévu de créer deux tables : une pour faciliter le nettoyage des 
-données (employees_temp) et une autre pour stocker les données nettoyées 
-(employees).
-"""
-
-create_employees_table = PostgresOperator(
-    task_id="create_employees_table",
-    postgres_conn_id="tutorial_pg_conn",
-    sql="""
-        CREATE TABLE IF NOT EXISTS employees (
-            "Serial Number" NUMERIC PRIMARY KEY,
-            "Company Name" TEXT,
-            "Employee Markme" TEXT,
-            "Description" TEXT,
-            "Leave" INTEGER
-        );""",
+dag = DAG(
+    dag_id="download_rocket_launches",
+    description="Download rocket pictures of recently launched rockets.",
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval="@daily",
 )
 
-create_employees_temp_table = PostgresOperator(
-    task_id="create_employees_temp_table",
-    postgres_conn_id="tutorial_pg_conn",
-    sql="""
-        DROP TABLE IF EXISTS employees_temp;
-        CREATE TABLE employees_temp (
-            "Serial Number" NUMERIC PRIMARY KEY,
-            "Company Name" TEXT,
-            "Employee Markme" TEXT,
-            "Description" TEXT,
-            "Leave" INTEGER
-        );""",
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
+    dag=dag,
 )
 
 
-#Tâche de récupération de données
-"""Ici, les données sont récupérées, enregistrées dans un fichier sur 
-l'instance Airflow, puis chargées à partir de ce fichier dans une table 
-intermédiaire afin de pouvoir exécuter les étapes de nettoyage des données."""
-@task
-def get_data():
-    # REMARQUE : configurez cela selon les besoins de votre environnement Airflow.
-    data_path = "/opt/airflow/dags/files/employees.csv"
-    os.makedirs(os.path.dirname(data_path), exist_ok=True)
+def _get_pictures():
+    # Ensure directory exists
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
 
-    url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
-
-    response = requests.request("GET", url)
-
-    with open(data_path, "w") as file:
-        file.write(response.text)
-
-    postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
-    conn = postgres_hook.get_conn()
-    cur = conn.cursor()
-    with open(data_path, "r") as file:
-        cur.copy_expert(
-            "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-            file,
-        )
-    conn.commit()
+    # Download all pictures in launches.json
+    with open("/tmp/launches.json") as f:
+        launches = json.load(f)
+        image_urls = [launch["image"] for launch in launches["results"]]
+        for image_url in image_urls:
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split("/")[-1]
+                target_file = f"/tmp/images/{image_filename}"
+                with open(target_file, "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded {image_url} to {target_file}")
+            except requests_exceptions.MissingSchema:
+                print(f"{image_url} appears to be an invalid URL.")
+            except requests_exceptions.ConnectionError:
+                print(f"Could not connect to {image_url}.")
 
 
+get_pictures = PythonOperator(
+    task_id="get_pictures", python_callable=_get_pictures, dag=dag
+)
 
-@task
-def merge_data():
-    query = """
-        INSERT INTO employees
-        SELECT *
-        FROM (
-            SELECT DISTINCT *
-            FROM employees_temp
-        ) t
-        ON CONFLICT ("Serial Number") DO UPDATE
-        SET
-              "Employee Markme" = excluded."Employee Markme",
-              "Description" = excluded."Description",
-              "Leave" = excluded."Leave";
-    """
-    try:
-        postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
-        conn = postgres_hook.get_conn()
-        cur = conn.cursor()
-        cur.execute(query)
-        conn.commit()
-        return 0
-    except Exception as e:
-        return 1
+notify = BashOperator(
+    task_id="notify",
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag,
+)
+
+download_launches >> get_pictures >> notify
